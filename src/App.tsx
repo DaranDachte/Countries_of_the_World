4151129d88b77781f11515fdd1bbe03422ccccf1
Daran Dachte <darandachte@gmail.com>
2023-07-14 21:02:02 +0200

    try&catch, card style, regions array were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 import { fetcher } from "./Helpers/fetcher";
 import { Country } from "./Types";
 
+import "./App.css";
+import Card from "./Components/Card/Card";
+
+const Regions: string[] = [
+  "Asia",
+  "Africa",
+  "Europe",
+  "America",
+  "Australia and Oceania",
+];
+
 function App() {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [activeCountry, setActiveCountry] = useState("");
+  const [error, setError] = useState("");
+
   const getData = async () => {
-    const data = await fetcher<Country[]>("https://restcountries.com/v3.1/all");
-    setCountries(data);
+    try {
+      const { data: categoriesFromAPI } = await fetcher<Country[]>(
+        "https://restcountries.com/v3.1/all"
+      );
+      setCountries(categoriesFromAPI);
+      setActiveCountry(categoriesFromAPI[0]);
+      setError("");
+    } catch (err) {
+      setError("Something goes wrong! 🙁");
+      setCountries([]);
+    } finally {
+      console.log("What happened?");
+    }
   };
 
   useEffect(() => {
     if (!countries.length) getData();
   });
   console.log(countries);
-  return <></>;
+
+  return (
+    <>
+      {Regions.map((region, index) => {
+        <Card
+          key={index}
+          flags={"flags"}
+          name={"name"}
+          capital={"capital"}
+          population={"population"}
+          onClick={"onClick"}
+        />;
+      })}
+    </>
+  );
 }
 
 export default App;
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import style from "./style.module.scss";
 
-const Card = () => {
-  return <div>Card</div>;
+import { Country } from "../../Types";
+
+const Card: React.FunctionComponent<Country> = ({
+  flags,
+  name,
+  capital,
+  population,
+  onClick,
+}) => {
+  return (
+    <div className={style.card}>
+      <div className={style.flag}>{flags}</div>
+      <div className={style.description}>
+        <h4>{name}</h4>
+        <p>Capital:{capital}</p>
+        <p>Population: {population}</p>
+        <p>Learn more: {onClick}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
