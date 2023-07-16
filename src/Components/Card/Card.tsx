41189cc205a0ed495dc5afb4cbab198de4c48662
Daran Dachte <darandachte@gmail.com>
2023-07-16 16:07:04 +0200

    fixed

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -2,24 +2,32 @@ import React from "react";
 import style from "./style.module.scss";
 
 import { Country } from "../../Types";
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
 
-const Card: React.FunctionComponent<Country> = ({
-  flags,
+type Props = {
+  flagUrl: string;
+  name: string;
+  capital: string;
+  population: number;
+  onClick: () => void;
+};
+
+const Card: React.FunctionComponent<Props> = ({
+  flagUrl,
   name,
   capital,
   population,
   onClick,
 }) => {
   return (
-    <div className={style.card}>
-      <div className={style.flag}>{flags}</div>
-      <div className={style.description}>
-        <h4>{name}</h4>
+    <figure className={style.card}>
+      <img src={flagUrl} alt={name} />
+      <figcaption>
         <p>Capital:{capital}</p>
         <p>Population: {population}</p>
-        <p>Learn more: {onClick}</p>
-      </div>
-    </div>
+        <ButtonComponent title="Learn more" onClick={onClick} />
+      </figcaption>
+    </figure>
   );
 };
 
--- src/Helpers/fetcher.ts
+++ src/Helpers/fetcher.ts
@@ -1,3 +1,10 @@
+/**
+ *  Здесь с помощью ассинхронной функции мы делаем запрос и получаем данные,
+ *  которые с помощью request.json() обрабатываем как json строку.
+ * @param url - адрес запроса
+ * @returns
+ */
+
 export const fetcher = async <T = any>(url: string): Promise<T> => {
   const request = await fetch(url);
   return request.json();
