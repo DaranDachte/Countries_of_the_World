bc8fe288d4006627e409b79ab0170df6957aadc9
Daran Dachte <darandachte@gmail.com>
2023-07-21 15:29:36 +0200

    searchfilter  was created

--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -1,12 +1,29 @@
-import React from "react";
 import style from "./style.module.scss";
 import { ReactComponent as IconSearch } from "../../assets/img/IconSearch.svg";
 
-const SearchForm = () => {
+type Props = {
+  onInput: (value: string) => void;
+  value: string;
+  placeholder?: string;
+};
+
+const SearchForm: React.FunctionComponent<Props> = ({
+  value,
+  onInput,
+  placeholder = "Search",
+}) => {
+  const SubmitHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onInput(e.target.value);
+  };
   return (
     <label className={style.searchForm}>
       <IconSearch />
-      <input className={style.input} placeholder="Search"></input>
+      <input
+        className={style.input}
+        placeholder={placeholder}
+        onInput={SubmitHandler}
+        value={value}
+      />
     </label>
   );
 };
