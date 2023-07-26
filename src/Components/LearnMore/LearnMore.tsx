9187ebad61b6f72084d10d2c2ff90f8f461d82f3
Daran Dachte <darandachte@gmail.com>
2023-07-26 19:34:13 +0200

    portal was created

---
+++ src/Components/LearnMore/LearnMore.tsx
@@ -0,0 +1,43 @@
+import { useState, useMemo } from "react";
+import style from "./LearnMore.module.scss";
+import { Country } from "../../Types";
+import ReactDOM from "react-dom";
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
+
+type Props = {
+  country: Country;
+  onClose: () => void;
+};
+
+const LearnMore: React.FunctionComponent<Props> = ({ country, onClose }) => {
+  const [isClose, setIsClose] = useState(false);
+  const asideClassName = useMemo(() => {
+    return isClose ? style.animationOut : style.animationIn;
+  }, [isClose]);
+
+  const closeHandler = () => {
+    setIsClose(true);
+    setTimeout(() => {
+      onClose();
+    }, 850);
+  };
+
+  return ReactDOM.createPortal(
+    <div className={style.learnMoreWrapper}>
+      <aside className={asideClassName}>
+        <div className={style.imgWrapper}>
+          <img src={country.flags.svg} alt={country.flags.alt} />
+          <ButtonComponent onClick={closeHandler} title={"Close"} />
+        </div>
+        <h2>{country.name.common}</h2>
+        <div className={style.inform}>
+          <p>Capital:{country.capital}</p>
+          <p>Population: {country.population.toLocaleString()}</p>
+        </div>
+      </aside>
+    </div>,
+    document.body
+  );
+};
+
+export default LearnMore;
